83e7c5ef45ad57203bf455df2ceeb34c5873fa01
Xiangyao Yu <yxy@draco1.csail.mit.edu>
2016-02-13 15:26:13 -0500

    [logging] more added to log files.

--- storage/log.cpp
+++ storage/log.cpp
@@ -1,7 +1,17 @@
 #include "log.h"
 
 void
-LogManager::logTxn( uint64_t txn_id, uint32_t length, char * after_image )
+LogManager::logTxn( uint64_t txn_id, uint64_t key, uint32_t length, char * after_image )
 {
+	// get a global LSN  
+	// generate a log record.
+	// put the log record into a buffer.
+	// if the buffer is full or times out, 
+	//    flush the buffer to disk
+	//    update the commit time for flushed transactions
+	// else
+	//    return
 	return;
 }
+
+
--- storage/log.h
+++ storage/log.h
@@ -7,9 +7,13 @@ class LogManager
 {
 public:
 	// flush the log to non-volatile storage
-	void logTxn( uint64_t txn_id, uint32_t length, char * after_image );
+	void logTxn( uint64_t txn_id, uint64_t key, uint32_t length, char * after_image );
 	// recover the database after crash
 	void recover();
 private:
+	// for normal operation
 	void flushLogBuffer();
+	// for recovery after crash
+	void readFromLog();
+	void redoLog(uint64_t key, uint32_t length, char * after_image); 
 };
