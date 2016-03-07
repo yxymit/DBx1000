16cfd6ae4489f2c6b8ece81b5f1b94af1b930a68
Xiangyao Yu <yxy@draco1.csail.mit.edu>
2016-03-06 20:49:16 -0500

    [logging] new logTxn

--- storage/log.h
+++ storage/log.h
@@ -8,7 +8,7 @@ class LogManager
 public:
     LogManager();
 	// flush the log to non-volatile storage
-	void logTxn( uint64_t txn_id, uint64_t key, uint32_t length, char * after_image );
+	void logTxn( uint64_t txn_id, uint32_t num_keys, string * table_names, uint64_t * keys, uint32_t * lengths, char ** after_image );
 	// recover the database after crash
 	void recover();
 private:
