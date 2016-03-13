18f48d88536b262735e1e417bcf3e7a03fd09bf8
Justin H. Kaashoek <justin.kaashoek@gmail.com>
2016-03-13 17:10:23 -0400

    Locking, account for table of txn, copy images, call flush after writing, append to end of log

--- storage/log.cpp
+++ storage/log.cpp
@@ -8,7 +8,9 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include<pthread.h>
 
+pthread_mutex_t lock;
 
 const int buff_size = 10;
 
@@ -17,9 +19,11 @@ uint64_t global_lsn = 0;
 struct log_record{
   uint64_t lsn;
   uint64_t txn_id;
-  uint64_t key;
-  uint64_t length;
-  char * after_image;
+  uint32_t num_keys;
+  string * table_names;
+  uint64_t * keys;
+  uint32_t * lengths;
+  char ** after_images;
 };
 
 struct log_record buffer[buff_size];
@@ -33,22 +37,36 @@ LogManager::LogManager()
 void 
 LogManager::logTxn( uint64_t txn_id, uint32_t num_keys, string * table_names, uint64_t * keys, uint32_t * lengths, char ** after_images )
 {
-  // TODO [YXY] Should lock the log manager
+  cout << "Entered logTxn";
+  // Lock log manager
+  pthread_mutex_lock(&lock);
+  
   uint64_t lsn = global_lsn;
   global_lsn ++;
+  buff_index ++;
   // generate a log record.
   // put the log record into a buffer.
   buffer[buff_index].lsn = lsn;
   buffer[buff_index].txn_id = txn_id;
-  // added [0] just to make it compile
-  buffer[buff_index].key = keys[0]; 	
-  buffer[buff_index].length = lengths[0];
-  // TODO [YXY] copy data, not ptr 
-  buffer[buff_index].after_image = after_images[0];
-  if (buff_index >= buff_size)
+  
+  buffer[buff_index].table_names = table_names;
+  buffer[buff_index].num_keys = num_keys;
+  buffer[buff_index].keys = keys; 	
+  buffer[buff_index].lengths = lengths;
+  // TODO [YXY] copy data, not ptr
+  buffer[buff_index].after_images = (char**) malloc(num_keys*sizeof(char *));
+  for (uint32_t a=0; a<num_keys; a++)
+    {
+      buffer[buff_index].after_images[a] = (char *) malloc(lengths[a]);
+      for (uint32_t b=0; b<lengths[a]; b++)
+	buffer[buff_index].after_images[a][b] = after_images[a][b];
+    }
+      
+  if (buff_index > buff_size)
     {
       flushLogBuffer();
     }
+  pthread_mutex_unlock(&lock);
   // if the buffer is full or times out, 
   //    flush the buffer to disk
   //    update the commit time for flushed transactions
@@ -60,16 +78,34 @@ LogManager::logTxn( uint64_t txn_id, uint32_t num_keys, string * table_names, ui
 void LogManager::flushLogBuffer()
 {
   ofstream log;
-  log.open("Log.data", ios::binary);
+  log.open("Log.data", ios::binary|ios::app);
   for (int i=0; i<buff_size; i++)
     {
-      // TODO [YXY] Flush the whole buffer at the same time.
-      log.write((char*)&buffer[i].lsn, sizeof(buffer[i].lsn));
-      log.write((char*)&buffer[i].txn_id, sizeof(buffer[i].txn_id));
-      log.write((char*)&buffer[i].key, sizeof(buffer[i].key));
-      log.write((char*)&buffer[i].length, sizeof(buffer[i].length));
-      log.write(buffer[i].after_image, buffer[i].length);
+      log.write((char *) &(buffer[i].lsn), sizeof(buffer[i].lsn));
+      log.write((char *) &(buffer[i].txn_id), sizeof(buffer[i].txn_id));
+      log.write((char *) &(buffer[i].num_keys), sizeof(buffer[i].num_keys));
+      for (uint32_t j=0; j<buffer[i].num_keys; j++)
+	{
+	  int n = buffer[i].table_names[j].length();
+	  log.write((char *) &n, sizeof(n));
+	  log.write((char *) &(buffer[i].table_names[j]), n); 
+	}
+      for (uint32_t j=0; j<buffer[i].num_keys; j++)
+	{
+	    log.write((char *) &(buffer[i].keys[j]), sizeof(buffer[i].keys[j]));
+	}
+    
+      for (uint32_t j=0; j<buffer[i].num_keys; j++)
+	{
+	  log.write((char *) &(buffer[i].lengths[j]), sizeof(buffer[i].lengths[j]));
+	}
+      
+      for (uint32_t j=0; j<buffer[i].num_keys; j++)
+	{
+	  log.write(buffer[i].after_images[j], buffer[i].lengths[j]);
+	}
     }
+  log.flush();
   log.close();
   buff_index = 0;
 }
