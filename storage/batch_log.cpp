35f0264e411c3a08285bf94b0d9f561754935ff7
Hongzhe Liu <liuhenry4428@draco1.csail.mit.edu>
2016-04-10 18:07:55 -0400

    added logTxn_batch

--- storage/batch_log.cpp
+++ storage/batch_log.cpp
@@ -1,2 +1,11 @@
 #include "batch_log.h"
+#include "log.h"
 
+const uint32_t num_loggers = 10;
+
+LogManager _loggers[num_loggers];
+
+void 
+BatchLog::logTxn_batch( uint64_t txn_id, uint32_t num_keys, string * table_names, uint64_t * keys, uint32_t * lengths, char ** after_images )
+{
+      _loggers[txn_id%num_loggers].logTxn( txn_id, num_keys , table_names,  keys,  lengths,  after_images );}
--- storage/batch_log.h
+++ storage/batch_log.h
@@ -1,5 +1,8 @@
 #pragma once
 
-class BatchLog {
+#include "global.h"
+#include "pthread.h"
 
+class BatchLog {
+   void logTxn_batch( uint64_t txn_id, uint32_t num_keys, string * table_names, uint64_t * keys, uint32_t * lengths, char ** after_images );
 };
