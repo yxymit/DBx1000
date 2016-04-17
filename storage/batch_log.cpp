7dc3c09dfd80ca6df8558e5aadb97ed13bd39fc8
Hongzhe Liu <liuhenry4428@draco1.csail.mit.edu>
2016-04-17 16:08:48 -0400

    added init funtion to batch_log

--- storage/batch_log.cpp
+++ storage/batch_log.cpp
@@ -5,9 +5,19 @@
 
 LogManager _loggers[num_loggers];
 
+void BatchLog::init()
+{
+   for (uint32_t i = 0; i< num_loggers; i++){
+      _loggers[i].init();
+    }
+     
+}
+
+
+
 void 
 BatchLog::logTxn_batch( uint64_t txn_id, uint32_t num_keys, string * table_names, uint64_t * keys, uint32_t * lengths, char ** after_images )
 {
-        // _loggers[txn_id%num_loggers].logTxn( txn_id, num_keys , table_names,  keys,  lengths,  after_images, (txn_id%num_loggers)  );
-        _loggers[txn_id%num_loggers].logTxn (txn_id, num_keys , table_names,  keys,  lengths,  after_images);
+         _loggers[txn_id%num_loggers].logTxn_batch( txn_id, num_keys , table_names,  keys,  lengths,  after_images, (txn_id%num_loggers)  );
+        //_loggers[txn_id%num_loggers].logTxn (txn_id, num_keys , table_names,  keys,  lengths,  after_images);
         }
--- storage/batch_log.h
+++ storage/batch_log.h
@@ -10,6 +10,7 @@ class BatchLog {
    public:
       void logTxn_batch( uint64_t txn_id, uint32_t num_keys, string * table_names, uint64_t * keys, uint32_t * lengths, char ** after_images );
 //         static const uint32_t num_loggers ;
+      void init ();
    private:
       LogManager _loggers[num_loggers];
 
