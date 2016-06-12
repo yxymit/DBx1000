f77ec1cf5a96fe1184da691784eb1c95742637d1
Annie <annie@AnniedeMacBook-Pro.local>
2016-06-12 18:02:21 -0400

    [parallel_log] implemented active set

--- storage/parallel_log.cpp
+++ storage/parallel_log.cpp
@@ -1,3 +1,4 @@
+#include "manager.h"
 #include "parallel_log.h"
 #include "log.h"
 #include <iostream>
@@ -10,128 +11,91 @@
 #include <string.h>
 #include <errno.h>
 #include <pthread.h>
+#include <vector>
+#include <unordered_set>
 
-/*struct log_record{
-  uint64_t lsn;
+struct wait_log_record{
+  char * log_entry;
+  uint32_t entry_size;
+  unordered_set<uint64_t> preds;
   uint64_t txn_id;
-  uint32_t num_keys;
-  string * table_names;
-  uint64_t * keys;
-  uint32_t * lengths;
-  char ** after_images;
-};*/
+  int thd_id;
+};
 
-int num_files, wait_freq;
-int log_file_id = 0;
-//int wait_count = 0;
+const static int WAIT_FREQ = 10;
+int wait_count;
+LogManager * _logger;
+vector<wait_log_record> wait_buffer;
 
 uint64_t * max_lsn;
-
-list<wait_log_record> wait_buffer;
-wait_log_record my_wait_log;
 
 ParallelLogManager::ParallelLogManager()
 {
   pthread_mutex_init(&lock, NULL);
-} //???
+} 
 
-void ParallelLogManager::init()
+void ParallelLogManager::init(int num_thd)
 {
-/*
-  cout << "number of log files: " << num_files;
-  //cout << "buffer size (parallel_log.cpp): " << buffer_size;
-  cout << "check wait_buffer frequency (parallel_log.cpp): " << wait_freq;
-  max_lsn = new uint64_t[num_files];
-  //para_global_lsn = new uint64_t[num_files];
-  string file_name;
-  _logger = new LogManager[num_files];
-  for(int i = 0; i < num_files; i++) {
-    file_name = "Log_" + to_string(i) + ".data";
-    _logger[i].init(file_name);
+  wait_count = 0;
+  wait_buffer = {};
+  _logger = new LogManager[num_thd];
+  for(int i = 0; i < num_thd; i++) {
+    _logger[i].init("Log_" + to_string(i) + ".data", num_thd);
   }
-  return;
-  */
 }
 
-bool ParallelLogManager::canParallelLog(uint64_t * lsn)
-{
-  for(int i = 0; i < num_files; i++)
-  {
-    if(lsn[i] > max_lsn[i])
-      return true;
+void ParallelLogManager::checkWait() {
+  vector<wait_log_record>::iterator tmp;
+  for (auto waitlog_it = wait_buffer.begin(); waitlog_it!= wait_buffer.end();) {
+    for (unsigned j = 0; j < waitlog_it->preds.bucket_count(); ++j) {
+      unordered_set<uint64_t>::iterator temp;
+      for (auto predtxnid = waitlog_it->preds.begin(j); predtxnid!= waitlog_it->preds.end(j);) {
+        if(!glob_manager->is_log_pending(*predtxnid)) {
+          temp = predtxnid;
+          ++temp;
+          waitlog_it->preds.erase(*predtxnid);
+          predtxnid = temp;
+        } else {
+          ++predtxnid;
+        }
+      }
+    }
+    if(waitlog_it->preds.empty()) {
+      tmp = waitlog_it;
+      ++tmp;
+      _logger[waitlog_it->thd_id % 4].logTxn(waitlog_it->log_entry, waitlog_it->entry_size);
+      // FLUSH DONE
+      glob_manager->remove_log_pending(waitlog_it->txn_id);
+      wait_buffer.erase(waitlog_it);
+      waitlog_it = tmp;
+    } else {
+      ++waitlog_it;
+    }
   }
-  return false;
 }
 
 void
-ParallelLogManager::parallelLogTxn(uint64_t txn_id, uint32_t num_keys, string * table_names, 
-  uint64_t * keys, uint32_t * lengths, char ** after_images, uint64_t * file_lsn)
+ParallelLogManager::parallelLogTxn(char * log_entry, uint32_t entry_size, uint64_t * pred, 
+  int pred_size, uint64_t txn_id, int thd_id)
 {
-  if(canParallelLog(file_lsn))
-  {
-    parallel_log(txn_id, num_keys, table_names, keys, lengths, after_images);
+  wait_log_record my_wait_log;
+  for(int i = 0; i < pred_size; i++) {
+    my_wait_log.preds.insert(pred[i]);
+  }
+  if(my_wait_log.preds.empty()) {
+    _logger[thd_id % 4].logTxn(log_entry, entry_size);
+    // FLUSH DONE
+    glob_manager->remove_log_pending(txn_id);
   } else {
-    wait_log(txn_id, num_keys, table_names, keys, lengths, after_images, file_lsn);
-  }
-  return;
-}
-
-void ParallelLogManager::wait_log(uint64_t txn_id, uint32_t num_keys, string * table_names, uint64_t * keys, 
-  uint32_t * lengths, char ** after_images, uint64_t * file_lsn)
-{
-/*  pthread_mutex_lock(&lock);
- 
-  my_wait_log.txn_id = txn_id;
-  my_wait_log.num_keys = num_keys;
-  
-  my_wait_log.keys = new uint64_t[num_keys];
-  my_wait_log.table_names = new string[num_keys];
-  my_wait_log.lengths = new uint32_t[num_keys];
-  my_wait_log.after_images = new char * [num_keys];
-  my_wait_log.file_lsn = new uint64_t[num_files];
-
-  for(int i = 0; i < num_files; i++) {
-    my_wait_log.file_lsn[i] = file_lsn[i];
-  }
-  for (uint32_t a=0; a<num_keys; a++)
-  {
-    my_wait_log.lengths[a] = lengths[a];
-    my_wait_log.keys[a] = keys[a];
-    my_wait_log.table_names[a] = table_names[a];
-    my_wait_log.after_images[a] = new char [lengths[a]];
-    for (uint32_t b=0; b<lengths[a]; b++)
-      my_wait_log.after_images[a][b] = after_images[a][b];
-  }
-  wait_buffer.push_back(my_wait_log);
-  
-  //check wait_buffer for updates
-  wait_count = (wait_count + 1) % wait_freq;
-  if(wait_count == 0) {
-    list<wait_log_record>::iterator i = wait_buffer.begin();
-    while (i != wait_buffer.end())
-    {
-      if(canParallelLog(i->file_lsn)) {
-        parallel_log(i->txn_id, i->num_keys, i->table_names, i->keys, i->lengths, 
-          i->after_images);
-        wait_buffer.erase(i++);
-      } else {
-        ++i;
-      }
+    my_wait_log.log_entry = log_entry;
+    my_wait_log.entry_size = entry_size;
+    my_wait_log.txn_id = txn_id;
+    my_wait_log.thd_id = thd_id;
+    wait_buffer.push_back(my_wait_log);
+    wait_count = (wait_count + 1) % WAIT_FREQ;
+    if(wait_count == 0) {
+      checkWait();
     }
+    _logger[thd_id % 4].logTxn(log_entry, entry_size);
   }
-  pthread_mutex_unlock(&lock);
-*/
 }
-
-void ParallelLogManager::parallel_log(uint64_t txn_id, uint32_t num_keys, string * table_names, 
-  uint64_t * keys, uint32_t * lengths, char ** after_images )
-{
-/*
-  _logger[log_file_id].logTxn(txn_id, num_keys, table_names, keys, lengths, after_images);
-  max_lsn[log_file_id] = _logger[log_file_id].getMaxlsn();
-  log_file_id = (log_file_id + 1) % num_files;
-  return;
-*/
-}
-
-
--- storage/parallel_log.h
+++ storage/parallel_log.h
@@ -3,23 +3,22 @@
 #include "global.h"
 #include "pthread.h"
 
+
 // ARIES style logging 
 class ParallelLogManager 
 {
-public:
-  ParallelLogManager();
-  void init();
-  void parallelLogTxn(uint64_t txn_id, uint32_t num_keys, string * table_names, 
-    uint64_t * keys, uint32_t * lengths, char ** after_images, uint64_t * file_lsn);
-  void wait_log(uint64_t txn_id, uint32_t num_keys, string * table_names, uint64_t * keys, 
-    uint32_t * lengths, char ** after_images, uint64_t * file_lsn);
-  bool canParallelLog(uint64_t * lsn);
+  public:
+    ParallelLogManager();
+    void init(int num_thd);
+    void parallelLogTxn(char * log_entry, uint32_t entry_size, uint64_t * pred, 
+      int pred_size, uint64_t txn_id, int thd_id);
+    //void wait_log(uint64_t txn_id, uint32_t num_keys, string * table_names, uint64_t * keys, 
+      //uint32_t * lengths, char ** after_images, uint64_t * file_lsn);
+    //bool canParallelLog(uint64_t * lsn);
 
-private:
-  void parallel_log(uint64_t txn_id, uint32_t num_keys, string * table_names, 
-    uint64_t * keys, uint32_t * lengths, char ** after_images);
-  //void flushLogBuffer();
-  ofstream log;
-  pthread_mutex_t lock;
-  //LogManager * _logger;
+  private:
+    void checkWait();
+    //void flushLogBuffer();
+    //ofstream log;
+    pthread_mutex_t lock;
 };
