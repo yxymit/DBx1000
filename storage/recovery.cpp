d658a79397fe508921c82f260c4fcf5478b2e72a
Justin H. Kaashoek <justin.kaashoek@gmail.com>
2016-06-29 20:29:36 -0400

    parallel recovery algorithm outline

--- storage/recovery.cpp
+++ storage/recovery.cpp
@@ -17,11 +17,13 @@
 #include <boost/lockfree/queue.hpp>
 
 // Algorithm 
-void ParallelLogManager::recovery()
+void ParallelLogManager::readFromLog(uint32_t & num_keys, string * &table_names, uint64_t * &keys, uint32_t * &lengths, char ** &after_image, uint64_t * file_lsn)
 {
 // counter for largest LSN that has been recovered
-
-// Begin recovery at smallest LSN
+  
+  // go through log files
+  // check that file is not empty
+  // go through each file in the txn
     // check any dependencies for that txn
         // if there is a 'hole'
              // cannot recovery anything with LSN greater than the current LSN, recovery process stops
@@ -29,4 +31,4 @@ void ParallelLogManager::recovery()
              // wait
         // otherwise
              // recover the txn and increment the largest LSN that has been recovered
-	     }
+}
