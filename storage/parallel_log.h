aa5414d6a95e44640d0e08458615c836304466dc
Annie <annie@AnniedeMacBook-Pro.local>
2016-07-10 16:46:26 -0700

    bugs

--- storage/parallel_log.h
+++ storage/parallel_log.h
@@ -16,12 +16,13 @@ class ParallelLogManager
     //void wait_log(uint64_t txn_id, uint32_t num_keys, string * table_names, uint64_t * keys, 
       //uint32_t * lengths, char ** after_images, uint64_t * file_lsn);
     //bool canParallelLog(uint64_t * lsn);
-    void recovery(uint32_t & num_keys, string * &table_names, uint64_t * &keys,  uint32_t * &lengths, char ** &after_image, uint64_t * file_lsn);
+   void recovery(uint32_t & num_keys, string * &table_names, uint64_t * &keys, uint32_t * &lengths, 
+    char ** &after_image, uint64_t &num_preds, uint64_t * &pred_txn_id, uint32_t thd_id);
     void runTxn();
 
   private:
     //void checkWait(int logger_id);
-	uint32_t get_logger_id(uint64_t thd_id) { return thd_id % NUM_LOGGER; } 
+  uint32_t get_logger_id(uint64_t thd_id) { return thd_id % NUM_LOGGER; } 
     //void flushLogBuffer();
     //ofstream log;
     pthread_mutex_t lock;
--- storage/recovery.cpp
+++ storage/recovery.cpp
@@ -1,4 +1,4 @@
- #include "manager.h"
+#include "manager.h"
 #include "parallel_log.h"
 #include "log.h"
 //#include <boost/thread/thread.hpp>                                            
@@ -17,39 +17,6 @@
 #include <log_pending_table.h>
 
 
-vector<uint64_t> preds;
+//vector<uint64_t> preds;
 
 // Algorithm 
-void ParallelLogManager::recovery(uint32_t & num_keys, string * &table_names, uint64_t * &keys, uint32_t * &lengths, 
-    char ** &after_image, uint64_t &num_preds, uint64_t * &pred_txn_id, uint32_t thd_id)
-{
-    uint64_t txn_id;
-    _logger[get_logger_id(thd_id)].readFromLog(&txn_id, &num_keys, &table_names, &keys, &lengths, &after_image, 
-      &num_preds, pred_txn_id);
-    bool can_recover = false;
-    preds.clear();
-    for(unsigned i = 0; i < num_preds; i++) {
-      preds.push_back(pred_txn_id[i]);
-    }
-    while(! can_recover) {
-        can_recover = true;
-        /*for(int i = 0; i < NUM_LOGGER; i++) {
-            if(recovery_lsn[i] < file_lsn[i])
-                can_recover = false;
-        }*/
-        for(auto i = preds.begin(); i != preds.end(); ) {
-          if(!recovered_txn.find(*i) != recovered_txn.end()) {
-            i++;
-            can_recover = false;
-          } else {
-            i = preds.erase(i);
-          }
-        }
-    }
-    pthread_mutex_lock(&lock);
-    runTxn();
-    recovered_txn.emplace(txn_id);
-    pthread_mutex_unlock(&lock);
-    //ATOM_ADD_FETCH(recovery_lsn[_logger_id], 1); 
-}
-
