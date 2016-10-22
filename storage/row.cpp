af7c662eaa859f6961a234930082e4a038a61cf5
Annie <annie@AnniedeMacBook-Pro.local>
2016-10-21 21:47:36 -0400

    fix

--- storage/row.cpp
+++ storage/row.cpp
@@ -169,7 +169,6 @@ row_t::get_data(txn_man * txn, access_t type)
 		new_version->next = _version;
 		new_version->txn_id = txn->get_txn_id();
 		new_version->ts = txn->getRecoverState()->commit_ts;
-		get_ts();
 		if(_version) {
 			memcpy(new_version->data, _version->data, get_tuple_size());
 		} else {
@@ -190,7 +189,7 @@ row_t::get_data(txn_man * txn, access_t type)
 				}
 				if(cur_version->ts < fence_ts && cur_version != _version) {
 					justbefore->next = cur_version->next;
-					delete cur_version->data;
+					//delete cur_version->data;
 				} else {
 					justbefore = cur_version;
 					min_ts = cur_version->ts;
--- system/txn.h
+++ system/txn.h
@@ -168,9 +168,8 @@ public:
 	uint64_t		last_writer;
 	PredecessorInfo * getPredecessorInfo() { return _predecessor_info; }
 	#if LOG_TYPE == LOG_COMMAND
-private:
-	RecoverState * _recover_state;
 public:
+	RecoverState * _recover_state;
 	RecoverState * getRecoverState() { return _recover_state; }
 	#endif
 #endif
