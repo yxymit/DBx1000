ee7cab0257ae5b840ea5a0a331f436d7188bdc5f
Xiangyao Yu <yxy@royal-01.cs.wisc.edu>
2020-02-19 20:00:59 -0600

    indentation fix

--- system/manager.cpp
+++ system/manager.cpp
@@ -66,9 +66,9 @@ ts_t Manager::get_min_ts(uint64_t tid) {
 	if (tid == 0 && now - last_time > MIN_TS_INTVL)
 	{ 
 		ts_t min = UINT64_MAX;
-    	for (UInt32 i = 0; i < g_thread_cnt; i++) 
-	    	if (*all_ts[i] < min)
-    	    	min = *all_ts[i];
+    		for (UInt32 i = 0; i < g_thread_cnt; i++)
+			if (*all_ts[i] < min)
+		    	    	min = *all_ts[i];
 		if (min > _min_ts)
 			_min_ts = min;
 	}
